b0bb63a90b1e8e88d68e37e094be598c2b83070d
PARAMHANS YADAV <100707185+param0595@users.noreply.github.com>
2023-01-24 22:54:26 +0530

    Updates

---
+++ precache-manifest.5614c49246c5e4f0c913ec4bb00fd7fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "96eb52adf2e8c6f37c66",
+    "url": "/Textutils-React/static/css/main.9b52e280.chunk.css"
+  },
+  {
+    "revision": "96eb52adf2e8c6f37c66",
+    "url": "/Textutils-React/static/js/main.96eb52ad.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Textutils-React/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Textutils-React/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "daf74f3886ef26676126",
+    "url": "/Textutils-React/static/js/runtime~main.daf74f38.js"
+  },
+  {
+    "revision": "0da4db9eee78d87e66c68fe5e973eb48",
+    "url": "/Textutils-React/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Textutils-React/precache-manifest.5614c49246c5e4f0c913ec4bb00fd7fe.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Textutils-React/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
